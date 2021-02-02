ea0715ab5047c65b20cf41d63738de785a40f364
Giovanni Lambertini <lambertini.giovanni1@gmail.com>
2021-02-02 17:00:26 +0100

    Updated ChargeStatusSensorResource

--- src/main/java/resource/ChargeStatusSensorResource.java
+++ src/main/java/resource/ChargeStatusSensorResource.java
@@ -6,6 +6,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.UUID;
 
 //This sensor depends on the presence of the vehicle
@@ -15,6 +17,10 @@ public class ChargeStatusSensorResource extends SmartObjectResource<ChargeStatus
     private static final Logger logger = LoggerFactory.getLogger(VehiclePresenceSensorResource.class);
 
     public static final String RESOURCE_TYPE = "iot:sensor:chargeStatus";
+
+    private static final long UPDATE_PERIOD = 10000; //10 Seconds
+
+    private static final long TASK_DELAY_TIME = 5000; //5 Seconds before starting the periodic update task
 
     private ChargeStatusDescriptor updatedChargeStatus;
 
@@ -45,6 +51,8 @@ public class ChargeStatusSensorResource extends SmartObjectResource<ChargeStatus
             Random random = new Random(System.currentTimeMillis());
             this.updatedChargeStatus = ChargeStatusDescriptor.UNPLUGGED;
 
+            startPeriodicEventValueUpdateTask();
+
         }catch (Exception e){
             logger.error("Error init ChargeStatusSensorResource ! Msg: {}", e.getLocalizedMessage());
         }
@@ -56,26 +64,53 @@ public class ChargeStatusSensorResource extends SmartObjectResource<ChargeStatus
         return this.updatedChargeStatus;
     }
 
+    private void startPeriodicEventValueUpdateTask() {
+        try{
+
+            logger.info("Starting periodic Update Task with Period: {} ms", UPDATE_PERIOD);
+
+            Timer updateTimer = new Timer();
+
+            updateTimer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    //logger.info("Updated Charge Status: {}", updatedChargeStatus);
+
+                    notifyUpdate(updatedChargeStatus);
+
+                }
+            }, TASK_DELAY_TIME, UPDATE_PERIOD);
+
+        }catch (Exception e){
+            logger.error("Error executing periodic update ! Msg: {}", e.getLocalizedMessage());
+        }
+    }
+
+    /**
+     * - VehiclePresenceSensor changed status, choose a new value for ChargeStatusSensor
+     */
     @Override
     public void onDataChanged(SmartObjectResource<Boolean> smartObjectResource, Boolean updatedValue) {
-        if (smartObjectResource != null && smartObjectResource.getType().equals(VehiclePresenceSensorResource.RESOURCE_TYPE) && updatedValue == true) {
-            logger.info("Vehicle detected by sensor: {}", smartObjectResource.getId());
+        if (smartObjectResource != null && smartObjectResource.getType().equals(VehiclePresenceSensorResource.RESOURCE_TYPE)) {
+            if (updatedValue == true) {     //If a new vehicle arrived, choose a random value
+                logger.info("Vehicle detected by sensor: {}", smartObjectResource.getId());
 
-            switch (random.nextInt(3)) {
-                case 0:
-                    updatedChargeStatus = ChargeStatusDescriptor.UNPLUGGED;
-                    break;
-                case 1:
-                    updatedChargeStatus = ChargeStatusDescriptor.PLUGGED;
-                    break;
-                case 2:
-                    updatedChargeStatus = ChargeStatusDescriptor.CHARGING;
-                    break;
+                switch (random.nextInt(3)) {
+                    case 0:
+                        updatedChargeStatus = ChargeStatusDescriptor.UNPLUGGED;
+                        break;
+                    case 1:
+                        updatedChargeStatus = ChargeStatusDescriptor.PLUGGED;
+                        break;
+                    case 2:
+                        updatedChargeStatus = ChargeStatusDescriptor.CHARGING;
+                        break;
+                }
+            }
+            else{       //If the parking lot is empty, put in UNPLUGGED status
+                updatedChargeStatus = ChargeStatusDescriptor.UNPLUGGED;
             }
 
         }
-        //logger.info("Updated Charge Status: {}", updatedChargeStatus);
-
-        notifyUpdate(updatedChargeStatus);
     }
 }
