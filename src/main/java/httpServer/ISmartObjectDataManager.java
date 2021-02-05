46754c88166e0b9e47acea12ffcdd252e898d113
Giovanni Lambertini <lambertini.giovanni1@gmail.com>
2021-02-05 18:03:51 +0100

    Updated Http Server

--- src/main/java/httpServer/ISmartObjectDataManager.java
+++ src/main/java/httpServer/ISmartObjectDataManager.java
@@ -8,14 +8,13 @@ import java.util.Optional;
 
 public interface ISmartObjectDataManager {
     //General Smart Object Manager
-    Optional<Map<String, SmartObject>> getSmartObjectList();                                //smartcity
+    Optional<Map<String, SmartObject>> getSmartObjectList();                              //smartcity
     Optional<GpsLocationDescriptor> getSmartObjectLocationList();                         //smartcity/gps
     Optional<SmartObject> getSmartObjectById(String id);                                  //smartcity/id_smart_object
     Optional<Map<String, SmartObject>> getSensorBySmartObjectId(String sensorType);       //smartcity/id_smart_object/sensor_type
 
     //Charging Station Manager
     Optional<Map<String, SmartObject>> getChargingStationList();                          //smartcity/parkinglot
-
 
     //Parking Lot Manager
     Optional<Map<String, SmartObject>> getParkingLotList();                               //smartcity/charging_station
--- src/main/java/httpServer/SmartObjectApi.java
+++ src/main/java/httpServer/SmartObjectApi.java
@@ -5,15 +5,14 @@ import dto.SmartObject;
 import io.dropwizard.jersey.errors.ErrorMessage;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import model.GpsLocationDescriptor;
 import model.SmartObjectTypeDescriptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import resource.SmartObjectResource;
 import services.AppConfig;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
@@ -38,7 +37,7 @@ public class SmartObjectApi {
     @Timed
     @Produces(MediaType.APPLICATION_JSON)
     @ApiOperation(value="Get all available Smart Objects")
-    public Response getLocations(@Context ContainerRequestContext req,
+    public Response getSmartObjects(@Context ContainerRequestContext req,
                                  @QueryParam("type") String type) {
 
         try {
@@ -60,10 +59,10 @@ public class SmartObjectApi {
             if(smartObjectType == null)
                 smartObjectList = this.conf.getInventoryDataManager().getSmartObjectList();
             else if(smartObjectType == SmartObjectTypeDescriptor.PARKING_LOT) {
-                smartObjectList = this.conf.getInventoryDataManager().getSmartObjectList();
+                smartObjectList = this.conf.getInventoryDataManager().getParkingLotList();
             }
             else if(smartObjectType == SmartObjectTypeDescriptor.CHARGING_STATION) {
-                smartObjectList = this.conf.getInventoryDataManager().getSmartObjectList();
+                smartObjectList = this.conf.getInventoryDataManager().getChargingStationList();
             }
             else
                 return Response.status(Response.Status.BAD_REQUEST).type(MediaType.APPLICATION_JSON_TYPE).entity(new ErrorMessage(Response.Status.BAD_REQUEST.getStatusCode(),"Type is not valid !")).build();
@@ -78,6 +77,96 @@ public class SmartObjectApi {
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).type(MediaType.APPLICATION_JSON_TYPE).entity(new ErrorMessage(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode(),"Internal Server Error !")).build();
         }
     }
+
+    @GET
+    @Path("/gps")
+    @Timed
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(value="Get all Gps Smart Objects Data")
+    public Response getGpsData(@Context ContainerRequestContext req ){
+
+        try {
+
+            logger.info("Loading all requested Gps Smart Objects Data");
+
+            Optional<GpsLocationDescriptor> gpsLocationDescriptor = this.conf.getInventoryDataManager().getSmartObjectLocationList();
+
+            if(!gpsLocationDescriptor.isPresent())
+                return Response.status(Response.Status.NOT_FOUND).type(MediaType.APPLICATION_JSON_TYPE).entity(new ErrorMessage(Response.Status.NOT_FOUND.getStatusCode(),"Smart Objects Not Found !")).build();
+
+            return Response.ok(gpsLocationDescriptor.get()).build();
+
+        } catch (Exception e){
+            e.printStackTrace();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).type(MediaType.APPLICATION_JSON_TYPE).entity(new ErrorMessage(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode(),"Internal Server Error !")).build();
+        }
+    }
+
+    @GET
+    @Path("/{id_smart_object}")
+    @Timed
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(value="Get Data about a Smart Objects")
+    public Response getSmartObjectById(@Context ContainerRequestContext req,
+                              @PathParam("id_smart_object") String idSmartObject) {
+
+        try {
+
+            logger.info("Loading Smart Object Info for id: {}", idSmartObject);
+
+            //Check the request
+            if(idSmartObject == null)
+                return Response.status(Response.Status.BAD_REQUEST).type(MediaType.APPLICATION_JSON_TYPE).entity(new ErrorMessage(Response.Status.BAD_REQUEST.getStatusCode(),"Invalid Smart Object Id Provided !")).build();
+
+            Optional<SmartObject> smartObject = this.conf.getInventoryDataManager().getSmartObjectById(idSmartObject);
+
+            if(!smartObject.isPresent())
+                return Response.status(Response.Status.NOT_FOUND).type(MediaType.APPLICATION_JSON_TYPE).entity(new ErrorMessage(Response.Status.NOT_FOUND.getStatusCode(),"Smart Object Id Not Found !")).build();
+
+            return Response.ok(smartObject.get()).build();
+
+        } catch (Exception e){
+            e.printStackTrace();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).type(MediaType.APPLICATION_JSON_TYPE).entity(new ErrorMessage(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode(),"Internal Server Error !")).build();
+        }
+    }
+
+    @GET
+    @Path("/{id_smart_object}/{sensor_type}")
+    @Timed
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(value="Get Data about a Smart Objects")
+    public Response getDevice(@Context ContainerRequestContext req,
+                              @PathParam("id_smart_object") String idSmartObject,
+                              @PathParam("sensor_type") String sensor_type){
+
+        try {
+
+            logger.info("Loading Smart Object Info for id: {}", idSmartObject);
+
+            //Check the request
+            if(idSmartObject == null)
+                return Response.status(Response.Status.BAD_REQUEST).type(MediaType.APPLICATION_JSON_TYPE).entity(new ErrorMessage(Response.Status.BAD_REQUEST.getStatusCode(),"Invalid Smart Object Id Provided !")).build();
+
+            Optional<SmartObject> smartObject = this.conf.getInventoryDataManager().getSmartObjectById(idSmartObject);
+
+            if(!smartObject.isPresent())
+                return Response.status(Response.Status.NOT_FOUND).type(MediaType.APPLICATION_JSON_TYPE).entity(new ErrorMessage(Response.Status.NOT_FOUND.getStatusCode(),"Smart Object Id Not Found !")).build();
+
+            SmartObject extractedSmartObject = smartObject.get();
+            SmartObjectResource<?> sensor = extractedSmartObject.getResourceMap().get(sensor_type);                  //Extract the requested sensor
+
+            if (sensor != null)
+                return Response.ok(sensor).build();
+            else
+                return Response.status(Response.Status.NOT_FOUND).type(MediaType.APPLICATION_JSON_TYPE).entity(new ErrorMessage(Response.Status.NOT_FOUND.getStatusCode(),"Sensor type Not Found !")).build();
+
+        } catch (Exception e){
+            e.printStackTrace();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).type(MediaType.APPLICATION_JSON_TYPE).entity(new ErrorMessage(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode(),"Internal Server Error !")).build();
+        }
+    }
+
 
 
 
