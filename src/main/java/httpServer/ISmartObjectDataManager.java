a2567450de6fd570c7853a258112d12770d6ae74
Alex Costa <costa98alex@gmail.com>
2021-02-05 18:43:52 +0100

    Average statistics completed

--- src/main/java/httpServer/ISmartObjectDataManager.java
+++ src/main/java/httpServer/ISmartObjectDataManager.java
@@ -15,10 +15,5 @@ public interface ISmartObjectDataManager {
     Optional<SmartObject> getSmartObjectById(String id);                                  //smartcity/id_smart_object
     Optional<Map<String, SmartObject>> getSensorBySmartObjectId(String sensorType);       //smartcity/id_smart_object/sensor_type
 
-    //Charging Station Manager
-   // Optional<Map<String, SmartObject>> getChargingStationList();                          //smartcity/parkinglot
-
-    //Parking Lot Manager
-    //Optional<Map<String, SmartObject>> getParkingLotList();                               //smartcity/charging_station
-    Optional<Map<String, SmartObject>> getSmartObjectsList(SmartObjectTypeDescriptor smartObjectType);
+    Optional<Map<String, SmartObject>> getSmartObjectsList(SmartObjectTypeDescriptor smartObjectType); //smartcity/parkinglot or //smartcity/charging_station
 }
